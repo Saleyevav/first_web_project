ab18a53ae191c97f37b76cf4343274dccd2dfd04
saleyevav <saleyevav@yandex.ru>
2017-11-15 16:11:23 +0300

    blabla2

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     body = [(i+'\n') for i in env['QUERY_STRING'].split('&')]
     #body = bytes(['kakaha\n','kozula'])
-    return body
+    return bytes(body)
