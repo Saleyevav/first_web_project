511329d41fcf496bfabf4bb903fd7bfa6be921cf
saleyevav <saleyevav@yandex.ru>
2017-11-26 19:08:15 +0300

    django

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello & 
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application & 
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #gunicorn -c /etc/gunicorn.d/test hello
